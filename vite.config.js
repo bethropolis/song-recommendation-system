539eab8756f371e6e1484001872e4d19b3861da7
Bethuel <bethropolis@gmail.com>
2023-11-08 04:41:48 +0300

    feat(pwa): added pwa support

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,49 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    VitePWA({
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      manifest: {
+        name: 'music recommendation',
+        short_name: 'musica',
+        description: 'a simple music recommendation app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'web/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'web/icon-192-maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: 'web/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'web/icon-512-maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ],
   optimizeDeps: {
     exclude: ['js-big-decimal']
   },
